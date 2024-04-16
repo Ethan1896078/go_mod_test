3e391403fbdc086011a7bf6ba1db0f1f8be77207
HuangZhe <huangzhe@HuangZhedeMacBook-Pro.local>
2024-04-16 14:15:30 +0800

    v1.0.0 release

--- pkg/hello/hello.go
+++ pkg/hello/hello.go
@@ -2,5 +2,5 @@ package hello
 import "fmt"
 
 func SayHello(){
-	fmt.Println("hello —— from github.com/Ethan1896078/go_mod_test/pkg/hello")
+	fmt.Println("hello —— from github.com/Ethan1896078/go_mod_test@v1.0.0")
 }
